3ba1cbef130c825416a9156249d554440b57cae8
Maarten Fuchs <mf828148@bose.com>
2023-04-01 01:29:19 +0200

    Initial commit

--- variables.tf
+++ variables.tf
@@ -1,22 +1,26 @@
 variable "subnet_id" {
   description = "Target subnet ID"
   type        = string
+  nullable    = false
 }
 
 variable "ami" {
   description = "AWS EC2 instance ami"
   type        = string
   default     = "ami-0b5eea76982371e91"
+  nullable    = false
 }
 
 variable "instance_type" {
   description = "AWS EC2 instance tye"
   type        = string
   default     = "t2.micro"
+  nullable    = false
 }
 
 variable "associate_public_ip_address" {
   description = "Enable/disable public ip address association"
   type        = bool
   default     = true
+  nullable    = false
 }
